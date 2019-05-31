1b10aeed9dcecb2f22462622e87a9dba011ebda1
Tom Morse <tom.morse@yale.edu>
2019-05-31 00:57:38 -0400

    "initial version"

---
+++ batch_back/back/mod/kvz_naz/mod_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+modl_reg(){
+	fprintf(stderr, "Additional mechanisms from files\n");
+
+fprintf(stderr," ca.mod");
+fprintf(stderr," cad.mod");
+fprintf(stderr," childa.mod");
+fprintf(stderr," iqq.mod");
+fprintf(stderr," kadist.mod");
+fprintf(stderr," kaprox.mod");
+fprintf(stderr," kca.mod");
+fprintf(stderr," km.mod");
+fprintf(stderr," kvz_nature.mod");
+fprintf(stderr," naz_nature.mod");
+fprintf(stderr," peak.mod");
+fprintf(stderr," syn2.mod");
+fprintf(stderr, "\n");
+_ca_reg();
+_cad_reg();
+_childa_reg();
+_iqq_reg();
+_kadist_reg();
+_kaprox_reg();
+_kca_reg();
+_km_reg();
+_kvz_nature_reg();
+_naz_nature_reg();
+_peak_reg();
+_syn2_reg();
+}
